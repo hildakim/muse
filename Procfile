a70e1fcd839c1908b680e15b5b5b01d9e260ccae
hildakim <pinkredjohn@gmail.com>
2021-08-13 03:23:57 +0900

    about release

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn muse.wsgi --log-file -
--- muse/settings.py
+++ muse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%v$myy^u#$ugs9069^fsj7mp1k*v3)^g_@o11ip+8gwo_w5iy%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -173,3 +174,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 DATETIME_INPUT_FORMATS = ['%Y-%m-%d %H:%M']
 DATE_INPUT_FORMATS = ['%Y-%m-%d']
 TIME_INPUT_FORMATS = ['%H:%M']
+
+# Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
