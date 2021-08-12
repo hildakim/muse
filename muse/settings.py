f3341753b7191e819c1f8fa20a6187950939d8ec
hildakim <pinkredjohn@gmail.com>
2021-08-13 04:04:15 +0900

    release commit

--- muse/settings.py
+++ muse/settings.py
@@ -179,3 +179,12 @@ TIME_INPUT_FORMATS = ['%H:%M']
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'muse-bucket'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
